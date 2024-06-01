7d0dbed727ff7e095b56ae922b5729e899a09aef
Marc Altmann <marcaltmann@posteo.de>
2024-06-01 18:57:19 +0200

    WIP

--- core/models.py
+++ core/models.py
@@ -3,7 +3,16 @@ from django.db import models
 
 class InputFile(models.Model):
     name = models.CharField(max_length=200)
-    content = models.JSONField()
+
+    def __str__(self):
+        return self.name
+
+
+class NamedEntity(models.Model):
+    label = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    timecode = models.DecimalField(max_digits=10, decimal_places=3,
+                                   null=False, blank=False)
 
     def __str__(self):
         return self.name
--- core/views.py
+++ core/views.py
@@ -3,7 +3,29 @@ from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 
 from core.forms import UploadFileForm
-from core.models import InputFile
+from core.models import InputFile, NamedEntity
+
+import json
+import time
+import spacy
+
+
+def do_ner(input_file: InputFile, transcript: dict):
+    nlp = spacy.load("de_core_news_lg")
+
+    with open('test.json') as f:
+        data = json.load(f)
+
+    for segment in data:
+        doc = nlp(segment['text'])
+        for ent in doc.ents:
+            entity = NamedEntity(
+                label=ent.label_,
+                name=ent.text,
+                timecode=segment['start'],
+                input_file=input_file,
+            )
+            entity.save()
 
 
 def index(request):
@@ -17,8 +39,9 @@ def upload(request):
             file = request.FILES['file']
             content_str = file.read()
             content = json.loads(content_str)
-            input_file = InputFile(name=file.name, content=content)
+            input_file = InputFile(name=file.name)
             input_file.save()
+            do_ner(input_file, content)
             return HttpResponseRedirect('/')
         else:
             return HttpResponseRedirect('/')
